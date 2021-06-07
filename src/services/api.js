727b588a35dcd8b007bf3aa62868431ab481aea3
Gabriel Alves Quadra <55445484+Zafyrer@users.noreply.github.com>
2021-06-07 00:59:24 -0300

    Mudanças PWA
    Atualização nos manifest (logo, nome, cores, orientação);
    Definido um tempo máximo para acessar o backend (10 segundos).

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
+    timeout: 10000,
     headers: {
         'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
         'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
