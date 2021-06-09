8c54b03ea002eab46a8f77c3da310dee9ba12ffc
Gabriel Alves Quadra <55445484+Zafyrer@users.noreply.github.com>
2021-06-09 11:28:05 -0300

    Update MeusSorteios
    Mudança no lugar que aparece o nome do ganhador.

--- src/views/components/MeusSorteios/index.jsx
+++ src/views/components/MeusSorteios/index.jsx
@@ -21,8 +21,7 @@ function MeusSorteios({ sorteios }) {
                                     <p className='card-text'><small className='text-muted'>{moment(sorteio.drawDate).fromNow()}</small></p>
                                 </>}
                                 {!sorteio.active && <>
-                                    <p className='card-text text-muted'>Ganhador</p>
-                                    <button className='btn btn-success btn-lg' disabled>{sorteio.winner.name}</button>
+                                    <p className='card-text text-muted'>Ganhador: {sorteio.winner.name}</p>
                                 </>}
                             </div>
                         </div>
