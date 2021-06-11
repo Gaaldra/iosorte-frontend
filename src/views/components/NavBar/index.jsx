af6f933a49ee79b7e99daae3ddb1057ce32b6970
Gabriel Alves Quadra <55445484+Zafyrer@users.noreply.github.com>
2021-06-11 00:37:17 -0300

    Update Navbar
    Tornando mais responsivo para celulares.

--- src/views/components/NavBar/index.jsx
+++ src/views/components/NavBar/index.jsx
@@ -13,26 +13,26 @@ function NavBar({ onButtonClick }) {
 
     return (
         <ul className="nav nav-pills justify-content-center">
-            <li className='nav-item mx-1'>
+            <li className='nav-item mx-sm-1'>
                 <button
                     className='nav-link text-white border active'
                     id='active'
                     value='0'
                     onClick={e => changePage(e)}>Sorteios</button>
             </li>
-            <li className='nav-item mx-1'>
+            <li className='nav-item mx-sm-1'>
                 <button
                     className='nav-link text-white border'
                     value='1'
-                    onClick={e => changePage(e)}>Meus tickets</button>
+                    onClick={e => changePage(e)}>Tickets</button>
             </li>
-            <li className='nav-item mx-1'>
+            <li className='nav-item mx-sm-1'>
                 <button
                     className='nav-link text-white border'
                     value='2'
-                    onClick={e => changePage(e)}>Meu perfil</button>
+                    onClick={e => changePage(e)}>Perfil</button>
             </li>
-            <li className='nav-item mx-1'>
+            <li className='nav-item mx-sm-1'>
                 <button
                     className='nav-link text-white border'
                     value='3'
